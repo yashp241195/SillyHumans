698796da81c6c3023ba48edcd1ae1742f70e1cde
Yash Pratap <yashp241195@gmail.com>
2022-05-17 09:45:10 +0530

    first commit

---
+++ data/members.js
@@ -0,0 +1,15 @@
+var allmembers = [
+    {
+        name:"Shubham Gupta",
+        dp:"https://media-exp1.licdn.com/dms/image/C4D03AQGx5dssQNWYlA/profile-displayphoto-shrink_800_800/0/1632375970904?e=1658361600&v=beta&t=XmBZKgSXogqd_ythuY7G3Q2lDtVrDDXXzcW_E7MBbHM",
+        tags:"#contentwriting #marketing",
+        headline:"Silly Human obsessed to scale your brand with Content Marketing Web3, Defi & Crypto Marketer Memes & Community FTW",
+        email:"#",
+        linkedin:"https://www.linkedin.com/in/shubhamgupta07/",
+        instagram:"#",
+        portfolio:"#",
+        description:`
+        Silly Human obsessed to scale your brand with Content Marketing Web3, Defi & Crypto Marketer Memes & Community FTW
+        `
+    },
+]
