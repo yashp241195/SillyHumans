0f611d78486eada818971464bdd50a3af362d948
Yash <yashp241195@gmail.com>
2022-05-23 14:44:44 +0530

    Update members.js

--- public/data/members.js
+++ public/data/members.js
@@ -1,15 +1,192 @@
 var allmembers = [
     {
-        name:"Shubham Gupta",
-        dp:"https://media-exp1.licdn.com/dms/image/C4D03AQGx5dssQNWYlA/profile-displayphoto-shrink_800_800/0/1632375970904?e=1658361600&v=beta&t=XmBZKgSXogqd_ythuY7G3Q2lDtVrDDXXzcW_E7MBbHM",
-        tags:"#contentwriting #marketing",
-        headline:"Silly Human obsessed to scale your brand with Content Marketing Web3, Defi & Crypto Marketer Memes & Community FTW",
-        email:"#",
-        linkedin:"https://www.linkedin.com/in/shubhamgupta07/",
-        instagram:"#",
-        portfolio:"#",
-        description:`
-        Silly Human obsessed to scale your brand with Content Marketing Web3, Defi & Crypto Marketer Memes & Community FTW
-        `
-    }
+      "name": "Kuldeep Gera", 
+      "tags": "#marketing #digitalmarketing #socialmedia #graphicdesigning", 
+      "headline": "Making marketing easy for businesses ", 
+      "description": "", 
+      "linkedin": "https://www.linkedin.com/in/kuldeep-gera/", 
+      "portfolio": "#", 
+      "email": "gera.prince.kuldeep@gmail.com", 
+      "dp": "https://media-exp1.licdn.com/dms/image/C5603AQENC0QZNcWcSw/profile-displayphoto-shrink_400_400/0/1621768576592?e=1658361600&v=beta&t=DZZo-iTlO2ub_0Qo1Kyp2x3xJA_meEeNt5rTuKSZZXk", 
+      "instagram": "https://www.instagram.com/wanderer_on_the_loose/"
+    },
+    {
+      name:"Shubham Gupta",
+      dp:"https://media-exp1.licdn.com/dms/image/C4D03AQGx5dssQNWYlA/profile-displayphoto-shrink_800_800/0/1632375970904?e=1658361600&v=beta&t=XmBZKgSXogqd_ythuY7G3Q2lDtVrDDXXzcW_E7MBbHM",
+      tags:"#contentwriting #marketing",
+      headline:"Silly Human obsessed to scale your brand with Content Marketing Web3, Defi & Crypto Marketer Memes & Community FTW",
+      email:"#",
+      linkedin:"https://www.linkedin.com/in/shubhamgupta07/",
+      instagram:"#",
+      portfolio:"#",
+      description:`
+      Silly Human obsessed to scale your brand with Content Marketing Web3, Defi & Crypto Marketer Memes & Community FTW
+      `
+  },
+  {
+    "name": "Aishwarya Dey", 
+    "tags": "#contentwriting #socialmedia #graphicdesigning", 
+    "headline": "I have a knack for visual storytelling", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/aishwaryadey24/", 
+    "portfolio": "https://linktr.ee/Aiishwarya", 
+    "email": "aishwaryad3y@gmail.com", 
+    "dp": "", 
+    "instagram": "https://www.instagram.com/aishcreateshere/"
+  }, 
+  {
+    "name": "Praneti Kore", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "When I decide to do something, I keep trying until it gets completed ", 
+    "description": "", 
+    "linkedin": "linkedin.com/in/pranetipkore", 
+    "portfolio": "campsite.bio/sociallypree", 
+    "email": "pranetipkore@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Salonee Thakkar", 
+    "tags": "#contentwriting", 
+    "headline": "I write creative, quirky and engaging content", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/saloneethakkar/", 
+    "portfolio": "https://drive.google.com/drive/folders/1hYkRiI1Jhbp_OzgI_IF6-eFuuKR18ACz?usp=sharing", 
+    "email": "thakkarsalonee7@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Keertika tiwari", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "I m a detail oriented person who substantiate pride in her work..\ud83c\udf1a\ud83c\udf1a\ud83c\udf1a", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/keertika-tiwari-849278206", 
+    "portfolio": ". ", 
+    "email": "Keertikatiwari45@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Krisha Bhupatani", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "Give me a task, ask me to do it and it'll be done at its best.", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/krisha-bhupatani-477626113/", 
+    "portfolio": "kafi bada h", 
+    "email": "krisha.bhupatani@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Shubham Sachdeva", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "Always curious to learn from people and Internet. ", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/shubham-s-798b2679/", 
+    "portfolio": "https://www.marketingmind.in/author/shubham", 
+    "email": "shubhamsachdeva16@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Hansika Tyagi", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "I don't give up easily on anything and everything.", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/hansika-tyagi-b87219219", 
+    "portfolio": "https://instagram.com/aryanpreneur?igshid=YmMyMTA2M2Y=", 
+    "email": "tyagihansika3@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Irfana Anjum", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "Work with a craftsman mindset \ud83e\udde0 and I take criticism positively. ", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/irfanaanjum/", 
+    "portfolio": "Tech niche - https://3dprintchamp.com/how-does-3d-printing-work-step-by-step-beginners-guide/", 
+    "email": "irfana.anjum.29@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Siddharth Sharma", 
+    "tags": "#contentwriting", 
+    "headline": "Arre B2B SaaS aur marketing content ke client bht khush hai. Content mein mastery chaalu hai. ", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/mwlite/in/siddharth-sharma-writer", 
+    "portfolio": "https://linktr.ee/createwithsid", 
+    "email": "siddsharma.work@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Nayonika Mazumdar", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "Hella punctual and Disciplined", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/nayonika-mazumdar", 
+    "portfolio": "https://drive.google.com/folderview?id=1-9T5vi2jYrtODbAduKDd0TcgrP9d6wfQ", 
+    "email": "nittingwordsbynano@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Mansi", 
+    "tags": "#contentwriting", 
+    "headline": "Neend aarhi hoti hai fir bhi kaam krti hu.. Isse best kya chahiye \ud83e\udd72", 
+    "description": "", 
+    "linkedin": "www.linkedin.com/in/mansi-content-writer", 
+    "portfolio": "linktr.ee/mansisondhi", 
+    "email": "msondhi60@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Nivithasri", 
+    "tags": "#contentwriting", 
+    "headline": "I write Blog posts ", 
+    "description": "", 
+    "linkedin": "www.linkedin.com/in/nivithasriS", 
+    "portfolio": "https://bit.ly/Nivithasriportfolio", 
+    "email": "nivithasri04@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Pranjali Goel", 
+    "tags": "#contentwriting", 
+    "headline": "Pro at getting the task done no matter what + making lame jokes. ", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/pranjali-goel", 
+    "portfolio": "https://linktr.ee/Pranjaligoel", 
+    "email": "contentcreatorpranjali@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  }, 
+  {
+    "name": "Shresth Shukla ", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "Good thing about me is I can accept my mistake if done any and would work on it. Better thing about me is I love to frame feelings into words. Best thing about me is that I love to those who love me and I also love to those who do not love me. I forgive people easily who put little efforts to make me smile. And that's the worst things fuck :) ", 
+    "description": "", 
+    "linkedin": "linkedin.com/in/shresthshuklaji ", 
+    "portfolio": "linktr.ee/theshresthshukla ", 
+    "email": "thisisshresth@gmail.com ", 
+    "dp": "", 
+    "instagram": "#"
+  },
+  {
+    "name": "Shikha ", 
+    "tags": "#contentwriting #socialmedia", 
+    "headline": "", 
+    "description": "", 
+    "linkedin": "https://www.linkedin.com/in/shikha-sehrawat/", 
+    "portfolio": "#", 
+    "email": "shikhasehrawat125@gmail.com", 
+    "dp": "", 
+    "instagram": "#"
+  },  
+
 ]
